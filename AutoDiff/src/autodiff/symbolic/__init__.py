53f7725f452b365b720999a3f93524b5f5e20297
TangJiahui <tangjiahui0610@gmail.com>
2020-12-08 01:21:35 +0800

    add more test case for symbolic

--- AutoDiff/src/autodiff/symbolic/__init__.py
+++ AutoDiff/src/autodiff/symbolic/__init__.py
@@ -1,4 +1,6 @@
-from .expression import Symbol
+from __future__ import annotations
+
+from .expression import Symbol, Expression
 
 
 def symbols(names: str):
@@ -8,11 +10,13 @@ def symbols(names: str):
     return Symbol(names)
 
 
-def diff(expression, respect_to):
-    if isinstance(expression, float):
+def diff(expr: Expression, *args):
+    if not isinstance(expr, Expression):
         # If it's a constant (not something wrapped by us), assume it's 0
         return 0
-    return expression._symdiff(respect_to)
+    for symbol in args:
+        expr = expr._symdiff(symbol)
+    return expr
 
 
 def get_jacobian_expression(expressions, respect_to_lst):
@@ -20,4 +24,4 @@ def get_jacobian_expression(expressions, respect_to_lst):
 
 
 def get_jacobian_value(expressions, respect_to_lst, values):
-    return [[diff(i, j).evaluate(values) for j in respect_to_lst] for i in expressions]
+    return [[col.evaluate(values) for col in row] for row in get_jacobian_expression(expressions, respect_to_lst)]
